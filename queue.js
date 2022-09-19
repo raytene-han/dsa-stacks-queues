3d7e8d18435ba533a64d9085b5be65076cec7fcf
Raytene <raytenehan@gmail.com>
2022-09-19 16:33:44 -0700

    completed queues and stacks

--- queue.js
+++ queue.js
@@ -20,26 +20,54 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    if (this.size === 0) {
+      this.first = new Node(val);
+      this.last = this.first;
+      this.size++;
+    } else {
+      this.last.next = new Node(val);
+      this.last = this.last.next;
+      this.size++;
+    }
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) {
+      throw new Error('cannot dequeue from empty queue');
+      return null;
+    }
 
+    if (this.size === 1) {
+      let temp = this.first.val;
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return temp;
+    }
+
+    let firstVal = this.first;
+    this.first = firstVal.next;
+    firstVal.next = null;
+    this.size--;
+
+    return firstVal.val
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    if (this.size === 0) return null;
 
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
--- stack.js
+++ stack.js
@@ -19,26 +19,34 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
-
+    let prev = this.top;
+    this.top = new Node(val);
+    this.top.next = prev;
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    let temp = this.top;
+    this.top = this.top.next;
+    this.size--;
 
+    return temp.val;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    if (this.size === 0) return null;
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
